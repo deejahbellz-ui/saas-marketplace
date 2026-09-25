21f46b8b68f7b5534c90bd35fa80988f239769ca
Your Name <you@example.com>
2026-09-25 09:04:36 +0100

    Point frontend to live Render backend

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
